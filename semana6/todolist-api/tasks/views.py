ca1178429143e19c694af829a4762557a7f6e130
Julio Vargas Tello <jvdark2020@gmail.com>
2024-06-20 19:54:29 -0500

    Django - React todolist

---
+++ semana6/todolist-api/tasks/views.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Category, Task
+from .serializers import CategorySerializer, TaskSerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
